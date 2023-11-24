8b351971ef859009d66d1d1c9d47f08618ed21f9
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-11-24 21:18:31 +0000

    shebangs

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-# Installs puppet-lint
+#!/usr/bin/pup
+# Installs Flask
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

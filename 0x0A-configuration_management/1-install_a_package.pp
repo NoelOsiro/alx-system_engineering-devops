1a3ea58fc29e74297cea9afb57de046ace18a1d8
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-11-24 21:09:08 +0000

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
-# Filename: install_flask.pp
+# Installs puppet-lint
 
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  require => Package['python3-pip'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }

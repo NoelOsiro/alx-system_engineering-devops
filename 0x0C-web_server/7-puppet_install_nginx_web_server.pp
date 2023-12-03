eb4567f7697d69de78c8409e832238140ea22269
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-12-03 13:58:04 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,20 @@
-# nginx_config.pp
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
+#setup nginx
+
+package {
+    'nginx':
+    ensure => installed,
 }
 
-# Define Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  hasstatus => true,
-  hasrestart => true,
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
 }
 
-# Configure Nginx site with root page
-file { '/var/www/html/index.html':
-  ensure  => present,
-  content => 'Hello World!',
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
 }
 
-# Create a Puppet template for the Nginx default site configuration
-file { '/etc/puppetlabs/code/environments/production/modules/nginx_config/templates/default.erb':
-  ensure => present,
-  content => '# This file is managed by Puppet
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 https://github.com/NoelOsiro;
-    }
+service {'nginx':
+    ensure => running,
 }
-',
-}
-
-# Create a symbolic link to enable the site
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
-}
-
-# End of manifest

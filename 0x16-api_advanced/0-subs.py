24d05d9f60edd0105a7e82358ed5d75eb83e7893
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-01-16 22:44:36 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""
-00subs.py - Get no fo subscribers to a topic
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Get no fo subscribers to a topic
-    """
-    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by NoelOsiro)"}
-    response = requests.get(api_url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
-    else:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""Function to print top 10 posts on a subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 top posts on a given subreddit."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by NoelOsiro)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 200:
-        # Parse the JSON response to get post information
-        data = response.json()
-
-        # Check if the 'data' key and 'children' key exist in the response
-        if 'data' in data and 'children' in data['data']:
-            # Extract and print the titles of the first 10 posts
-            for i, post in enumerate(data['data']['children'][:10]):
-                print(f"{post['data']['title']}")
-        else:
-            # Print None if the required keys are not present
-            print("None")
-    else:
-        # Print None if the request was not successful
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]

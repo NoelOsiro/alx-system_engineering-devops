8f58a2516a6857d6c7d8d671faaa4761aa7ea4ef
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-01-17 01:05:36 +0300

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+00subs.py - Get no fo subscribers to a topic
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'CustomBot/1.0'}
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+    else:
+        return 0

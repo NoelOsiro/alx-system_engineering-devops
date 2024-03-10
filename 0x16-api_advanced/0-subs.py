425e804c504c7d0bbc55357adbd4c31339d2f933
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-03-10 20:56:46 +0000

    added headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ Function that queries the Reddit API and returns the number of subscribers
 If an invalid subreddit is given, the function should return 0
 """
 
+import base64
 import requests
 
 
@@ -13,11 +14,13 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
-
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent': 'alx-pass/1.0',
+        'Authorization': f'Basic {base64.b64encode(
+            b"Rb1PJ7q615K2oGNI871S4w:jaUSqXlK0P4nFdEGleLCoe9SpgrF2w")
+            .decode("utf-8")}'
+    }
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
     else:
